9b7a1c13b012dcba4c9e82d72e67b1cc95bfc153
tauqeerahmad5201 <hellotauqeer@gmail.com>
2025-01-12 18:37:00 +0530

    added base src/ file

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>My First Spheron App</h1>
+        <p>
+          This app is deployed on Spheron
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
